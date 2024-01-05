a210effb756473a143ac0a72a6577e523ce752b8
Frank A. Krueger <fak@praeclarum.org>
2024-01-05 14:25:52 -0800

    Inline pbytesIndexOf

--- Program.fs
+++ Program.fs
@@ -1,6 +1,4 @@
-﻿
-
-// For more information see https://aka.ms/fsharp-console-apps
+﻿// 1 Billion Row Challenge in F#
 
 open System
 open System.Buffers.Text
@@ -71,7 +69,7 @@ let baseline (measurementsPath : string) =
 
 #nowarn "9"
 
-let pbytesIndexOf (pbytes : nativeptr<byte>) (length: int64) (v : byte) : int64 =
+let inline pbytesIndexOf (pbytes : nativeptr<byte>) (length: int64) (v : byte) : int64 =
     let mutable i: int64 = 0
     let mutable run = true
     while run && i < length do
@@ -113,89 +111,14 @@ let mmapPtr (measurementsPath : string) =
     }
     let mutable p = filePtr
     let mutable plength = fileLength
-    let semicolon: byte = ';' |> byte
-    let newline: byte = '\n' |> byte
     while plength > 0 do
-        match pbytesIndexOf p plength newline with
+        match pbytesIndexOf p plength '\n'B with
         | -1L ->
             printfn "FAILED TO FIND NEWLINE"
             plength <- 0
         | newlineIndex when newlineIndex > 0 ->
             let lineLength: int64 = newlineIndex
-            match pbytesIndexOf p lineLength semicolon with
-            | -1L ->
-                printfn "FAILED TO FIND SEMICOLON"
-                plength <- 0
-            | semicolonIndex ->
-                let name = utf8.GetString(p, int semicolonIndex)
-                let temp = parseTemp (NativePtr.add p (int semicolonIndex + 1))
-                match stations.TryGetValue(name, &entry) with
-                | true ->
-                    stations.[name] <- {
-                        Min = min entry.Min temp
-                        Max = max entry.Max temp
-                        Sum = entry.Sum + temp
-                        Count = entry.Count + 1
-                    }
-                | false ->
-                    stations.[name] <- {
-                        Min = temp
-                        Max = temp
-                        Sum = temp
-                        Count = 1
-                    }
-                count <- count + 1
-                p <- NativePtr.add p (int newlineIndex + 1)
-                plength <- plength - (newlineIndex + 1L)
-        | _ ->
-            plength <- 0
-            
-        if count > 0 && (count % 10000000) = 0 then
-            let entriesPerSecond = (float count) / stopwatch.Elapsed.TotalSeconds
-            let estimatedTotalTime = TimeSpan.FromSeconds (1.0e9 / entriesPerSecond)
-            printfn "Processed %d lines (plength=%A) (est %O)" count plength estimatedTotalTime
-    let sortedStations =
-        stations
-        |> Seq.sortBy (fun (kv : KeyValuePair<string, StationData>) -> kv.Key)
-    let mutable head = "{"
-    for station in sortedStations do
-        let e = station.Value
-        printf "%s%s=%.1f/%.1f/%.1f" head station.Key e.Min (e.Sum / double e.Count) e.Max
-        head <- ", "
-    printfn "}"
-
-let mmapPtrThreaded (measurementsPath : string) (numThreads : int) =
-    let utf8 = System.Text.Encoding.UTF8
-    let mmap = MemoryMappedFiles.MemoryMappedFile.CreateFromFile(measurementsPath, FileMode.Open)
-    let mmapA = mmap.CreateViewAccessor()
-    let mutable filePtr: nativeptr<byte> = NativePtr.nullPtr<byte>
-    mmapA.SafeMemoryMappedViewHandle.AcquirePointer(&filePtr)
-    let fileLength = mmapA.Capacity
-    
-    let nominalChunkLength = int (fileLength / int64 numThreads)
-    let mutable chunkPtr = filePtr
-    
-    let stations = Dictionary<string, StationData>()
-    let mutable count = 0
-    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
-    let mutable entry: StationData = {
-        Min = 0.0
-        Max = 0.0
-        Sum = 0.0
-        Count = 0
-    }
-    let mutable p = filePtr
-    let mutable plength = fileLength
-    let semicolon: byte = ';' |> byte
-    let newline: byte = '\n' |> byte
-    while plength > 0 do
-        match pbytesIndexOf p plength newline with
-        | -1L ->
-            printfn "FAILED TO FIND NEWLINE"
-            plength <- 0
-        | newlineIndex when newlineIndex > 0 ->
-            let lineLength: int64 = newlineIndex
-            match pbytesIndexOf p lineLength semicolon with
+            match pbytesIndexOf p lineLength ';'B with
             | -1L ->
                 printfn "FAILED TO FIND SEMICOLON"
                 plength <- 0
@@ -239,4 +162,3 @@ let mmapPtrThreaded (measurementsPath : string) (numThreads : int) =
 
 // baseline measurementsPath
 mmapPtr measurementsPath
-// mmapPtrThreaded measurementsPath 10
